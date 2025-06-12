2a84d953f51dc1e915e82a05ddf16f4c1e91cf60
modigulhasan <gulhasan_modi@msn.com>
2025-06-12 19:06:09 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,9 @@
+#Task 1
+a=input('Enter First Value: ')
+b=input('Enter Second Value: ')
+a=int(a)
+b=int(b)
+print('Addition: ',a + b)
+print('Subtraction: ',a - b)
+print('Multiplication: ',a * b)
+print('Division: ',a / b)
---
+++ Task 2.py
@@ -0,0 +1,4 @@
+#Task 2
+a=input('Please Enter First Name: ')
+b=input('Please Enter Last  Name: ')
+print('Hello '+a+' '+b+'!'+' Welcome to Python program.')
